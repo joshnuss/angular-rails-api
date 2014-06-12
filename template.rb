8307933049c307929585053dce1d7c321391c84f
Josh Nussbaum <joshnuss@gmail.com>
2014-06-12 09:14:49 -0400

    Added /bower_components to config.ru

--- template.rb
+++ template.rb
@@ -35,7 +35,7 @@ file "config.ru", <<-CODE
 require ::File.expand_path('../config/environment',  __FILE__)
 
 use Rack::Static,
-  urls: ["/views", "/images", "/scripts", "/styles"],
+  urls: ["/views", "/images", "/scripts", "/styles", "/bower_components"],
   index: 'index.html',
   root: "public"
 
